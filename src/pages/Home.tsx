8d564217d2685169ea1fb310ac0af9fa830e7ab2
Jay Lee <randomdori@gmail.com>
2026-01-31 17:33:30 -0800

    update home to dashboard like

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,43 @@
 
-import { Container, Header, ContentLayout } from '@cloudscape-design/components';
+import {
+    Header,
+    Grid,
+    SpaceBetween,
+    ContentLayout
+} from "@cloudscape-design/components";
+import SystemHealth from "../components/SystemHealth";
+import ResourceOverview from "../components/ResourceOverview";
+import RuntimeConfig from "../components/RuntimeConfig";
+import EventLog from "../components/EventLog";
+import UsageLimits from "../components/UsageLimits";
 
-export default function Home() {
+export default function Dashboard() {
+
     return (
         <ContentLayout
             header={
-                <Header variant="h1">
-                    Welcome to My Portfolio
-                </Header>
+                <Header variant="h1">Dashboard</Header>
             }
         >
-            <Container>
-                <p>Hi, I am Jay Lee. I am a software engineer.</p>
-                <p>Explore my work and get in touch!</p>
-            </Container>
+            <SpaceBetween size="l">
+                {/* 1. SERVICE HEALTH (Your Availability) */}
+                <SystemHealth />
+
+                {/* 2. RESOURCE OVERVIEW (Hard Skills Metrics) */}
+                <ResourceOverview />
+
+
+                {/* 3. BOTTOM ROW: ACTIVITY LOG & PERSONALITY STATS */}
+                <Grid gridDefinition={[{ colspan: { default: 12, s: 8 } }, { colspan: { default: 12, s: 4 } }]}>
+                    {/* LEFT: Recent Activity Log */}
+
+                    <RuntimeConfig />
+
+                    {/* RIGHT: Cost & Usage (Personality) */}
+                    <UsageLimits />
+                </Grid>
+                <EventLog />
+            </SpaceBetween>
         </ContentLayout>
     );
 }
