ca0656e6ae6075606ff19019762d116e6004796c
Jay Lee <randomdori@gmail.com>
2026-02-04 08:29:38 -0800

    replace div with box

--- src/components/ResourceOverview.tsx
+++ src/components/ResourceOverview.tsx
@@ -13,32 +13,25 @@ export default function ResourceOverview() {
             <ColumnLayout columns={3} variant="text-grid">
                 <Box>
                     <Box variant="awsui-key-label">Total Experience</Box>
-                    <div style={{ fontSize: "24px", fontWeight: "bold" }}>2 Years</div>
+                    <Box fontSize="heading-xl" fontWeight="bold" >2 Years</Box>
                     <Box variant="small" color="text-body-secondary">
                         AWS
                     </Box>
                 </Box>
-                {/* <div>
-                                    <Box variant="awsui-key-label">Certifications</Box>
-                                    <div style={{ fontSize: "24px", fontWeight: "bold" }}>1</div>
-                                    <Box variant="small" color="text-body-secondary">
-                                        PCEP-30-02 (Python)
-                                    </Box>
-                                </div> */}
-                <div>
+                <Box>
                     <Box variant="awsui-key-label">Primary Stack</Box>
-                    <div style={{ fontSize: "24px", fontWeight: "bold" }}>React</div>
+                    <Box fontSize="heading-xl" fontWeight="bold" >React</Box>
                     <Box variant="small" color="text-body-secondary">
                         Next.js / TypeScript / Cloudscape
                     </Box>
-                </div>
-                <div>
+                </Box>
+                <Box>
                     <Box variant="awsui-key-label">Repositories</Box>
-                    <div style={{ fontSize: "24px", fontWeight: "bold" }}>20+</div>
+                    <Box fontSize="heading-xl" fontWeight="bold" >20+</Box>
                     <Box variant="small" color="text-body-secondary">
                         GitHub & Private Projects
                     </Box>
-                </div>
+                </Box>
             </ColumnLayout>
         </Container>
     )
