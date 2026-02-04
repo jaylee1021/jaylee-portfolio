554cda212bc48418f4f86759e5cab0ec63d616ec
Jay Lee <randomdori@gmail.com>
2026-02-04 09:31:07 -0800

    change columnlayout to keyvaluepairs

--- src/components/SystemHealth.tsx
+++ src/components/SystemHealth.tsx
@@ -1,5 +1,5 @@
 
-import { Container, Header, ColumnLayout, Box, StatusIndicator, Link } from "@cloudscape-design/components";
+import { Container, Header, StatusIndicator, Link, KeyValuePairs } from "@cloudscape-design/components";
 
 export default function SystemHealth() {
     return (
@@ -13,32 +13,35 @@ export default function SystemHealth() {
                 </Header>
             }
         >
-            <ColumnLayout columns={3} variant="text-grid">
-                <Box>
-                    <Box variant="awsui-key-label">Status</Box>
-                    <StatusIndicator type="success">Available for Hire</StatusIndicator>
-                </Box>
-                <Box>
-                    <Box variant="awsui-key-label">Role</Box>
-                    <Box variant="p">Frontend Engineer</Box>
-                </Box>
-                <Box>
-                    <Box variant="awsui-key-label">Latest Incident</Box>
-                    <Box variant="p">Amazon Layoff (Resolved)</Box>
-                </Box>
-                <Box>
-                    <Box variant="awsui-key-label">Location</Box>
-                    <Box variant="p">Seattle, WA</Box>
-                </Box>
-                <Box>
-                    <Box variant="awsui-key-label">Phone number</Box>
-                    <Box variant="p">253-355-6520</Box>
-                </Box>
-                <Box>
-                    <Box variant="awsui-key-label">Email</Box>
-                    <Link href="mailto:leejayjong@gmail.com">leejayjong@gmail.com</Link>
-                </Box>
-            </ColumnLayout>
+            <KeyValuePairs
+                columns={3}
+                items={[
+                    {
+                        label: "Status",
+                        value: <StatusIndicator type="success">Available for Hire</StatusIndicator>
+                    },
+                    {
+                        label: "Role",
+                        value: "Frontend Engineer"
+                    },
+                    {
+                        label: "Latest Incident",
+                        value: "Amazon Layoff (Resolved)"
+                    },
+                    {
+                        label: "Location",
+                        value: "Seattle, WA"
+                    },
+                    {
+                        label: "Phone number",
+                        value: "253-355-6520"
+                    },
+                    {
+                        label: "Email",
+                        value: <Link href="mailto:leejayjong@gmail.com">leejayjong@gmail.com</Link>
+                    }
+                ]}>
+            </KeyValuePairs>
         </Container>
     )
 }   
